efb644ac7fc15643fe620a1cd502c2b24aa1b00a
William Yang <williamyang2017@gmail.com>
2019-09-12 23:12:29 +1000

    Added socket view

---
+++ views/Socket.py
@@ -0,0 +1,16 @@
+from PySide2 import QtGui, QtCore, QtWidgets
+
+class Socket(QtWidgets.QGraphicsItem):
+    def __init__(self, parent, radius=10):
+        super().__init__(parent)
+        self._rect = QtCore.QRect(0, 0, 2*radius, 2*radius)
+    
+    def boundingRect(self):
+        return self._rect
+    
+    def paint(self, painter, option, widget):
+        painter.setBrush(QtGui.QBrush())
+        painter.setPen(QtGui.QPen())
+
+        painter.drawEllipse(self.boundingRect())
+
