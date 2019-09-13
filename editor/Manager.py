10aec896579d76f31039378f568f0353e945a1fb
William Yang <williamyang2017@gmail.com>
2019-09-13 16:04:34 +1000

    Refactored manager to use new label loader

--- editor/Manager.py
+++ editor/Manager.py
@@ -2,10 +2,8 @@ from PySide2 import QtGui, QtCore, QtWidgets
 
 from views import Renderer
 from .Browser import Browser
-from models import JSONSerialiser
-from script_parser import parse_lines
+from .LabelsLoader import LabelsLoader
 
-import json
 import sys
 import os
 
@@ -13,46 +11,24 @@ class Manager:
     def __init__(self, organiser, view):
         self.organiser = organiser
         self.view = view
-        self.browser = Browser(view) 
+        self.browser = Browser(self, view) 
         self.renderer = Renderer(organiser, self.browser)
-        self.serialiser = JSONSerialiser()
-        self.explored_files = {}
+        self.loader = LabelsLoader()
+    
+    def findExternalLabel(self, label):
+        filepath = self.loader.getLabelFilepath(label)
+        if filepath:
+            self.openFile(filepath)
+            self.browser.findLabel(label)
     
     def openFile(self, filepath):
-        labels = self.cacheFile(filepath)
+        labels = self.loader.loadFromFilepath(filepath)
         if len(labels) > 0:
             self._displayLabels(labels)
     
     def cacheFile(self, filepath):
-        if os.path.isdir(filepath):
-            return self._loadFromDirectory(filepath)
-        elif os.path.isfile(filepath):
-            return self._loadFromFile(filepath)
-        return []
+        self.loader.loadFromFilepath(filepath) 
 
-    def _loadFromDirectory(self, directory):
-        labels = []
-        for filename in os.listdir(directory):
-            filepath = os.path.join(directory, filename)
-            if os.path.isdir(filepath):
-                labels.extend(self._loadFromDirectory(filepath))
-            else:
-                labels.extend(self._loadFromFile(filepath))
-        return labels
-    
-    def _loadFromFile(self, filepath):
-        if filepath in self.explored_files:
-            return self.explored_files.get(filepath)
-
-        try:
-            with open(filepath, "r", encoding="utf8") as fp:
-                labels = parse_lines(fp.readlines())
-        except:
-            labels = []
-
-        self.explored_files.setdefault(filepath, labels)
-        return labels        
-    
     def _displayLabels(self, labels):
         self.clear()
         for label in labels:
@@ -66,7 +42,4 @@ class Manager:
         self.organiser.clear()
         self.browser.clear()
     
-    def saveModel(self, filepath):
-        data = [label.accept(serialiser) for label in self.labels]
-        with open(filepath, "w", encoding="utf8") as fp:
-            json.dump(data, fp, indent=2)
+    
