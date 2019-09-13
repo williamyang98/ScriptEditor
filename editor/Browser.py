fc6506036f7f3e743e923f843fb6115a93c00253
William Yang <williamyang2017@gmail.com>
2019-09-13 15:39:35 +1000

    Incoporated new browser

--- editor/Browser.py
+++ editor/Browser.py
@@ -1,6 +1,17 @@
+from PySide2 import QtCore
+
 class Browser:
-    def __init__(self):
+    def __init__(self, view):
         self.labels = {}
+        self.view = view
+
+    def clear(self):
+        self.view.scene().clear()
+        self.centreOn(QtCore.QPointF(0, 0))
+        self.labels = {}
+    
+    def addItem(self, item):
+        self.view.scene().addItem(item)
 
     def addLabel(self, label, view):
         self.labels.setdefault(label, view)
@@ -8,8 +19,7 @@ class Browser:
     def findLabel(self, label):
         view = self.labels.get(label)
         if view is not None:
-            print("goto {0}".format(label))
             self.centreOn(view.pos())
 
     def centreOn(self, position):
-        print("centre to {0}".format(position.x(), position.y()))
+        self.view.centerOn(position)
--- editor/Manager.py
+++ editor/Manager.py
@@ -10,31 +10,61 @@ import sys
 import os
 
 class Manager:
-    def __init__(self, organiser, scene):
+    def __init__(self, organiser, view):
         self.organiser = organiser
-        self.scene = scene
-        self.browser = Browser() 
-        self.renderer = Renderer(scene, organiser, self.browser)
+        self.view = view
+        self.browser = Browser(view) 
+        self.renderer = Renderer(organiser, self.browser)
         self.serialiser = JSONSerialiser()
-        self.labels = []
+        self.explored_files = {}
     
-    def loadFromFile(self, filepath):
+    def openFile(self, filepath):
+        labels = self.cacheFile(filepath)
+        if len(labels) > 0:
+            self._displayLabels(labels)
+    
+    def cacheFile(self, filepath):
+        if os.path.isdir(filepath):
+            return self._loadFromDirectory(filepath)
+        elif os.path.isfile(filepath):
+            return self._loadFromFile(filepath)
+        return []
+
+    def _loadFromDirectory(self, directory):
+        labels = []
+        for filename in os.listdir(directory):
+            filepath = os.path.join(directory, filename)
+            if os.path.isdir(filepath):
+                labels.extend(self._loadFromDirectory(filepath))
+            else:
+                labels.extend(self._loadFromFile(filepath))
+        return labels
+    
+    def _loadFromFile(self, filepath):
+        if filepath in self.explored_files:
+            return self.explored_files.get(filepath)
+
+        try:
+            with open(filepath, "r", encoding="utf8") as fp:
+                labels = parse_lines(fp.readlines())
+        except:
+            labels = []
+
+        self.explored_files.setdefault(filepath, labels)
+        return labels        
+    
+    def _displayLabels(self, labels):
         self.clear()
-
-        with open(filepath, "r", encoding="utf8") as fp:
-            self.labels = parse_lines(fp.readlines())
-        
-        for label in self.labels:
+        for label in labels:
             label.accept(self.renderer)
-
-        self.organiseView() 
+        self.organiseView()
     
     def organiseView(self):
         self.organiser.organise()
 
     def clear(self):
         self.organiser.clear()
-        self.scene.clear()
+        self.browser.clear()
     
     def saveModel(self, filepath):
         data = [label.accept(serialiser) for label in self.labels]
